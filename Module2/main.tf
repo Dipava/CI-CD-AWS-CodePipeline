cf83fef27cee0283d9866a959f56027405ff16bb
Dipava <dipavo.roy@gmail.com>
2022-11-12 15:18:47 +0530

    create_cloudwatch_log_group=false

--- Module2/main.tf
+++ Module2/main.tf
@@ -22,7 +22,7 @@ module "rdsdb" {
   maintenance_window              = "Mon:00:00-Mon:03:00"
   backup_window                   = "03:00-06:00"
   enabled_cloudwatch_logs_exports = ["general"]
-  create_cloudwatch_log_group     = true
+  create_cloudwatch_log_group     = false
   create_random_password     = false
   backup_retention_period = 0
   skip_final_snapshot     = true
